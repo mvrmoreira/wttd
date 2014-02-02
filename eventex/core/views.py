33cd0c4a524ea63de4c341894416b190d36c8955
Matheus Moreira <mvrmoreira@gmail.com>
2014-02-02 20:49:52 -0200

    renderizando templates

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
+from django.template import loader, Context
 
 def home(request):
-	return HttpResponse('Bem-vindo ao EventeX!')
+	t = loader.get_template('index.html')
+	c = Context()
+
+	content = t.render(c)
+
+	return HttpResponse(content)
