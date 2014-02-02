020705980968173eb6f566fe55d33bb135f487f1
Matheus Moreira <mvrmoreira@gmail.com>
2014-02-02 21:02:23 -0200

    adicionando static files

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render_to_response
+from django.conf import settings
 
 def home(request):
-	return render_to_response('index.html')
+	context = {'STATIC_URL': settings.STATIC_URL}
+	return render_to_response('index.html', context)
