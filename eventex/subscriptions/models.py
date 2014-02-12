5b69c8993c914c6155d7ad92ab0da83f9ce6a531
Matheus Moreira <mvrmoreira@gmail.com>
2014-02-11 23:35:35 -0200

    criando models with tests

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Subscription(models.Model):
+    name = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=11, unique=True)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.name
---
+++ eventex/subscriptions/tests/test_models.py
@@ -0,0 +1,59 @@
+# coding: utf-8
+from django.test import TestCase
+from django.db import IntegrityError
+from datetime import datetime
+from eventex.subscriptions.models import Subscription
+
+
+class SubscriptionTest(TestCase):
+    def setUp(self):
+        self.obj = Subscription(
+            name='Matheus Moreira',
+            cpf='01234567890',
+            email='matheus@moreira.com.br',
+            phone='(21) 98888-7777'
+        )
+
+    def testCreate(self):
+        'Subscription must have name, cpf, email, phone'
+        self.obj.save()
+        self.assertEqual(1, self.obj.pk)
+
+    def testHasCreatedAt(self):
+        'Subscription must have automatic created_at'
+        self.obj.save()
+        self.assertIsInstance(self.obj.created_at, datetime)
+
+    def testUnicode(self):
+        self.assertEqual(u'Matheus Moreira', unicode(self.obj))
+
+
+class SubscriptionUniqueTest(TestCase):
+    def setUp(self):
+        # Create a first entry to force collision
+        Subscription.objects.create(
+            name='Matheus Moreira',
+            cpf='01234567890',
+            email='matheus@moreira.com.br',
+            phone='(21) 98888-7777'
+        )
+
+    def testCPFUnique(self):
+        'CPF must be unique'
+        s = Subscription(
+            name='Matheus Moreira',
+            cpf='01234567890',
+            email='outro@moreira.com.br',
+            phone='(21) 98888-7777'
+        )
+        self.assertRaises(IntegrityError, s.save)
+
+    def testEmailUnique(self):
+        'Email must be unique'
+        s = Subscription(
+            name='Matheus Moreira',
+            cpf='99999999999',
+            email='matheus@moreira.com.br',
+            phone='(21) 98888-7777'
+        )
+        self.assertRaises(IntegrityError, s.save)
