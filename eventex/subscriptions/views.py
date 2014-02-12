f0613e750e1d1c69d607016aced07f37216287e9
Matheus Moreira <mvrmoreira@gmail.com>
2014-02-12 00:08:51 -0200

    refatorando view

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -7,11 +7,17 @@ from eventex.subscriptions.models import Subscription
 
 def subscribe(request):
     if request.method == 'POST':
-        form = SubscriptionForm(request.POST)
-        if form.is_valid():
-            obj = form.save()
-            return HttpResponseRedirect('/inscricao/%d/' % obj.pk)
-        else:
-            return render(request, 'subscriptions/subscription_form.html', {'form': form})
+        return create(request)
     else:
-        return render(request, 'subscriptions/subscription_form.html', {'form': SubscriptionForm()})
+        return new(request)
+
+def new(request):
+    return render(request, 'subscriptions/subscription_form.html', {'form': SubscriptionForm()})
+
+def create(request):
+    form = SubscriptionForm(request.POST)
+    if form.is_valid():
+        obj = form.save()
+        return HttpResponseRedirect('/inscricao/%d/' % obj.pk)
+    else:
+        return render(request, 'subscriptions/subscription_form.html', {'form': form})
