8c0f732308fe34db63f2ea15f288681c169a7dd8
Matheus Moreira <mvrmoreira@gmail.com>
2014-02-21 16:37:17 -0300

    refatorando o url conf

--- eventex/urls.py
+++ eventex/urls.py
@@ -8,9 +8,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'eventex.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', 'eventex.core.views.home', name='home'),
-    url(r'^inscricao/$', 'eventex.subscriptions.views.subscribe', name='subscribe'),
-    url(r'^inscricao/(\d+)/$', 'eventex.subscriptions.views.detail', name='detail'),
-
+    url(r'^inscricao/', include('eventex.subscriptions.urls', namespace='subscription')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('eventex.core.urls', namespace='core')),
 )
